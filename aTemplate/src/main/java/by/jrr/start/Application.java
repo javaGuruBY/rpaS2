c702cb2f8af4e48cb15b3a9d919b65aa2e0c9402
anpoliakov <Andrewlybimenko@gmail.com>
2021-01-17 18:47:07 +0300

    add a few code and changed the file .gitignore

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -3,6 +3,11 @@ package by.jrr.start;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World and java guru!");
+
+    }
+
+    public static void fewCode(){
+        System.out.println("a few lines of code");
     }
 }
