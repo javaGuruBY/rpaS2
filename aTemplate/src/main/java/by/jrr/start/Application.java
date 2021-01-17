1ff3be66ac57613fef6f11609b7ef5f6c6d474bf
anpoliakov <Andrewlybimenko@gmail.com>
2021-01-17 18:59:44 +0300

    added a small method

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World and java guru!");
-
+        fewCode();
     }
 
     public static void fewCode(){
