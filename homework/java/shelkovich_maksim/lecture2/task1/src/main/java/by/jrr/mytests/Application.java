fb567c71227b5085c2d82cc2b2d32b650437f515
max-java <6666350@gmail.com>
2021-01-20 20:45:41 +0300

    adds junit tests

--- homework/java/shelkovich_maksim/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
+++ homework/java/shelkovich_maksim/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
@@ -1,11 +1,8 @@
 package by.jrr.mytests;
 
-import by.jrr.mytests.service.TestService;
-
 public class Application {
 
-    public static void main(String[] args) throws Exception {
-        TestService testService = new TestService();
-        testService.run();
+    public static void main(String[] args){
+
     }
 }
---
+++ homework/java/shelkovich_maksim/lecture2/task1/src/test/java/by/jrr/mytests/service/DogServiceUtilTest.java
@@ -0,0 +1,20 @@
+package by.jrr.mytests.service;
+
+import by.jrr.mytests.bean.Dog;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DogServiceUtilTest {
+
+    @Test
+    public void bark() {
+        Dog dog = new Dog();
+        dog.setName("Malysh");
+        String expected = "Malysh";
+
+        String actual =  DogServiceUtil.bark(dog);
+
+        assertEquals(expected, actual);
+    }
+}
