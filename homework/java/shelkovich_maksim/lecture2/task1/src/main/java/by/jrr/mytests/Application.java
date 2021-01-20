e8f4a64fbdafee35badbd53af95450354906aebd
max-java <6666350@gmail.com>
2021-01-20 20:01:44 +0300

    adds Dog feature

---
+++ homework/java/shelkovich_maksim/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
@@ -0,0 +1,20 @@
+package by.jrr.mytests;
+
+import by.jrr.mytests.bean.Dog;
+import by.jrr.mytests.service.DogService;
+import by.jrr.mytests.service.DogServiceUtil;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.setName("Malysh");
+
+        System.out.println(DogServiceUtil.bark(dog));
+
+        DogService dogService = new DogService(dog);
+        System.out.println(dogService.bark());
+
+
+    }
+}
---
+++ homework/java/shelkovich_maksim/lecture2/task1/src/main/java/by/jrr/mytests/service/DogServiceUtil.java
@@ -0,0 +1,10 @@
+package by.jrr.mytests.service;
+
+import by.jrr.mytests.bean.Dog;
+
+public class DogServiceUtil {
+
+    public static String bark (Dog dog) {
+        return dog.getName() + " BARK!";
+    }
+}
