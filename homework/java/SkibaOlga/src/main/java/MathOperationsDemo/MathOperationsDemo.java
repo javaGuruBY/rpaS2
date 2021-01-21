db66f33f7a1739096e66fb57c1891fe64c16129f
olgaz-skiba <olgaz.sweetheart@gmail.com>
2021-01-21 22:55:13 +0200

    adds initial project for lecture2 task1: test

---
+++ homework/java/SkibaOlga/src/main/java/MathOperationsDemo/MathOperationsDemo.java
@@ -0,0 +1,26 @@
+package MathOperationsDemo;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the 'Math Operations'!");
+
+        int a = 12;
+        int b = 4;
+
+        System.out.println("1) Addition '+':");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a + b = " + a + " + " + b + " = " + (a+b));
+
+        System.out.println("2) Subtraction '-':");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a - b = " + a + " - " + b + " = " + (a-b));
+
+        System.out.println("3) Multiplication '*':");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a * b = " + a + " * " + b + " = " + (a*b));
+
+        System.out.println("4) Division '/':");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a / b = " + a + " / " + b + " = " + (a / b));
+    }
+}
