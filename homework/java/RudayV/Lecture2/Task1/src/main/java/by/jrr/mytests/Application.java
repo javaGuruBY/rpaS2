6ee40270f7842665c150d790d8ede7cc187fda7d
Volodymyr <77403989+i3oi3ka@users.noreply.github.com>
2021-01-22 17:10:12 +0200

    adds dog feature

---
+++ homework/java/RudayV/Lecture2/Task1/src/main/java/by/jrr/mytests/Application.java
@@ -0,0 +1,17 @@
+package by.jrr.mytests;
+
+import by.jrr.mytests.bean.Dog;
+import by.jrr.mytests.service.DogService;
+import by.jrr.mytests.service.DogServiceUtil;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.setName("Kolya");
+        System.out.println(DogServiceUtil.bark(dog));
+
+        DogService dogService = new DogService(dog);
+        System.out.println(dogService.bark());
+    }
+}
