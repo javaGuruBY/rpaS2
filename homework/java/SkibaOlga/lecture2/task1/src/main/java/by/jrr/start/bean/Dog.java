2464c97a7eee26385db04f519ec1d06b5c090c17
olgaz-skiba <olgaz.sweetheart@gmail.com>
2021-01-23 22:27:19 +0200

    adds Test feature

--- homework/java/SkibaOlga/lecture2/task1/src/main/java/by/jrr/start/bean/Dog.java
+++ homework/java/SkibaOlga/lecture2/task1/src/main/java/by/jrr/start/bean/Dog.java
@@ -7,26 +7,32 @@ public class Dog {
     double price;
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
 
--- homework/java/SkibaOlga/lecture2/task1/src/main/java/by/jrr/start/service/DogServiceUtil.java
+++ homework/java/SkibaOlga/lecture2/task1/src/main/java/by/jrr/start/service/DogServiceUtil.java
@@ -3,6 +3,7 @@ package by.jrr.start.service;
 import by.jrr.start.bean.Dog;
 
 public class DogServiceUtil {
+
     public static String bark(Dog dog) {
         return dog.getName() + " BARK!";
     }
