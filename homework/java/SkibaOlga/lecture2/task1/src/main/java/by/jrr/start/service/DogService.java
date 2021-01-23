14e58d1df25817af1ac598618386fef848624a78
olgaz-skiba <olgaz.sweetheart@gmail.com>
2021-01-23 10:49:48 +0200

    adds Dog feature

---
+++ homework/java/SkibaOlga/lecture2/task1/src/main/java/by/jrr/start/service/DogService.java
@@ -0,0 +1,16 @@
+package by.jrr.start.service;
+
+import by.jrr.start.bean.Dog;
+
+public class DogService {
+
+    Dog dog;
+
+    public DogService(Dog dog) {
+        this.dog = dog;
+    }
+
+    public String bark() {
+        return dog.getName() + " BARK!";
+    }
+}
