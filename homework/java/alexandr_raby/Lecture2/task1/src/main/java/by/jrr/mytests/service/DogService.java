6473984c1cbc43aa6a4dbd82701bb4313fc02e09
Alexandr Raby <riabyi.alexandr@gmail.com>
2021-01-23 16:55:36 +0300

    adds Dog feature

---
+++ homework/java/alexandr_raby/Lecture2/task1/src/main/java/by/jrr/mytests/service/DogService.java
@@ -0,0 +1,16 @@
+package by.jrr.mytests.service;
+
+import by.jrr.mytests.bean.Dog;
+
+public class DogService {
+
+    Dog dog;
+
+    public DogService(Dog dog) {
+        this.dog = dog;
+    }
+
+    public String bark() {
+        return dog.getName() + " BARK!";
+    }
+}
