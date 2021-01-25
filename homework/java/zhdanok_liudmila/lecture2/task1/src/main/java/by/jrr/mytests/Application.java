c9a45e8573de130625d2d0e3fef06035dbaf016d
LiudmilaZhdanok <zhdanokla@gmail.com>
2021-01-26 02:03:02 +0300

    changed after maven

--- homework/java/zhdanok_liudmila/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
+++ homework/java/zhdanok_liudmila/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
@@ -1,9 +1,6 @@
 package by.jrr.mytests;
 
-import by.jrr.mytests.bean.Dog;
-import by.jrr.mytests.servise.DogServise;
-import by.jrr.mytests.servise.DogServiseUtil;
-import by.jrr.mytests.servise.TestService;
+
 
 public class Application {
 
--- homework/java/zhdanok_liudmila/lecture2/task1/src/test/java/by/jrr/mytests/servise/DogServiseTest.java
+++ homework/java/zhdanok_liudmila/lecture2/task1/src/test/java/by/jrr/mytests/servise/DogServiseTest.java
@@ -1,7 +1,7 @@
 package by.jrr.mytests.servise;
 
 import by.jrr.mytests.bean.Dog;
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
