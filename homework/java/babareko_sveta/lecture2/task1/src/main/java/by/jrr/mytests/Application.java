84d1de8aac13dd786397fa3666d8bebd9466b158
Asus <sveta.romasho@inbox.ru>
2021-01-26 15:44:17 +0300

    Fixed Application.java

--- homework/java/babareko_sveta/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
+++ homework/java/babareko_sveta/lecture2/task1/src/main/java/by/jrr/mytests/Application.java
@@ -3,7 +3,7 @@ package by.jrr.mytests;
 import by.jrr.mytests.bean.Dog;
 import by.jrr.mytests.service.DogService;
 import by.jrr.mytests.service.DogServiceUtill;
-import by.jrr.mytests.service.TestService;
+
 
 public class Application {
 
