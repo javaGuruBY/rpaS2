8a3a59e148ad8eb8551ce1181da16b205a1cbf74
Yury Fyodorov <fedoruch2000@gmail.com>
2021-01-26 14:18:20 +0300

    add junit tests

--- homework/java/fyodorov/lecture2/task1/src/main/java/by/jrr/mytest/Application.java
+++ homework/java/fyodorov/lecture2/task1/src/main/java/by/jrr/mytest/Application.java
@@ -2,12 +2,11 @@ package by.jrr.mytest;
 import by.jrr.mytest.bean.Dog;
 import by.jrr.mytest.service.DogService;
 import by.jrr.mytest.service.DogServiceUtil;
-import by.jrr.mytest.service.TestService;
+
 
 public class Application {
 
-    public static void main(String[] args) throws Exception{
-        TestService testService = new TestService();
-        testService.run();
+    public static void main(String[] args) {
+
     }
 }
--- homework/java/fyodorov/lecture2/task1/src/main/java/by/jrr/mytest/service/DogService.java
+++ homework/java/fyodorov/lecture2/task1/src/main/java/by/jrr/mytest/service/DogService.java
@@ -12,4 +12,11 @@ public class DogService {
     public String bark (){
         return dog.getName () + " BARK!";
     }
+    public double convertPriceToBelarus (double exchangeRate){
+        double convertedPrice = dog.getPrice()*exchangeRate;
+        if (convertedPrice > 1000){
+            convertedPrice = 0.97 * convertedPrice;
+        }
+        return convertedPrice;
+    }
 }
