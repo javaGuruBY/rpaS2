7b1d6ff1963ba878f4866aacda0134ce5adc1d74
Elena <len.owa@yandex.ru>
2021-01-26 20:33:14 +0300

    adds Test feature

--- homework/java/Owa_E/HW1L2/src/main/java/by/jrr/start/Application.java
+++ homework/java/Owa_E/HW1L2/src/main/java/by/jrr/start/Application.java
@@ -3,14 +3,12 @@ package by.jrr.start;
 import by.jrr.start.bean.Dog;
 import by.jrr.start.service.DogService;
 import by.jrr.start.service.DogServiceUtil;
+import by.jrr.start.service.TestService;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.setName("MSH");
-        System.out.println(DogServiceUtil.bark(dog));
-        DogService dogService = new DogService(dog);
-        System.out.println(dogService.bark());
+    public static void main(String[] args) throws Exception {
+        TestService testService = new TestService();
+        testService.run();
     }
 }
--- homework/java/Owa_E/HW1L2/src/main/java/by/jrr/start/service/DogServiceUtil.java
+++ homework/java/Owa_E/HW1L2/src/main/java/by/jrr/start/service/DogServiceUtil.java
@@ -7,3 +7,4 @@ public class DogServiceUtil {
         return dog.getName()+" BARK!";
     }
 }
+
