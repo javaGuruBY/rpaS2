297926b7dcabf699046089baddb578426623d684
olgaz-skiba <olgaz.sweetheart@gmail.com>
2021-01-26 13:05:48 +0200

    adds checkUserPassword functionality

--- homework/java/SkibaOlga/loginFeature/src/main/java/by/jrr/start/service/LoginService.java
+++ homework/java/SkibaOlga/loginFeature/src/main/java/by/jrr/start/service/LoginService.java
@@ -1,5 +1,10 @@
 package by.jrr.start.service;
 
+import by.jrr.start.bean.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- homework/java/SkibaOlga/loginFeature/src/test/java/by/jrr/start/service/LoginServiceTest.java
+++ homework/java/SkibaOlga/loginFeature/src/test/java/by/jrr/start/service/LoginServiceTest.java
@@ -1,8 +1,44 @@
 package by.jrr.start.service;
 
-import static org.junit.Assert.*;
+import by.jrr.start.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
 
+    LoginService loginService;
+
+    @Before
+    public void setUp(){
+        this.loginService = new LoginService();
+    }
+
+    @Test
+    public void checkUserPassword_positive() {
+        User user = getUser();
+
+        String userInput = "password";
+
+        boolean actuelResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actuelResult);
+
+    }
+
+    @Test
+    public void checkUserPassword_negative() {
+        User user = getUser();
+
+        String userInput = "wrong";
+
+        boolean actuelResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actuelResult);
+
+    }
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 
 }
