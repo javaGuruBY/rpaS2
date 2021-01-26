a1949b2ac09c610af02cebcc9a4e19b28fb41616
Alexandr Raby <riabyi.alexandr@gmail.com>
2021-01-26 18:03:37 +0300

    adds initial attemps value

--- homework/java/alexandr_raby/LoginFeature/src/test/java/by/jrr/service/LoginServiceTest.java
+++ homework/java/alexandr_raby/LoginFeature/src/test/java/by/jrr/service/LoginServiceTest.java
@@ -9,15 +9,19 @@ import org.junit.Test;
 public class LoginServiceTest {
 
     LoginService loginService;
+    User user;
+
 
     @Before
     public void setUp() {
         this.loginService = new LoginService();
+        this.user = getUser();
+        Assert.assertEquals(3, getUser().getLoginAttempts());
+
     }
 
     @Test
     public void checkUserPassword_positive() {
-        User user = getUser();
 
         String userInput = "password";
 
@@ -27,7 +31,6 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPassword_negative() {
-        User user = getUser();
 
         String userInput = "wrong";
 
