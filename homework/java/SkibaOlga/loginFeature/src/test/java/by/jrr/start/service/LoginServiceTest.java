58cfaad39ad58f7b2ad1dae69dd7b821681baf57
olgaz-skiba <olgaz.sweetheart@gmail.com>
2021-01-26 23:31:49 +0200

    if user blocked, he can't login

--- homework/java/SkibaOlga/loginFeature/src/test/java/by/jrr/start/service/LoginServiceTest.java
+++ homework/java/SkibaOlga/loginFeature/src/test/java/by/jrr/start/service/LoginServiceTest.java
@@ -9,6 +9,7 @@ public class LoginServiceTest {
 
     LoginService loginService;
     User user;
+    User blockedUser;
     String positiveUserInput = "password";
     String negativeUserInput = "wrong";
 
@@ -16,8 +17,13 @@ public class LoginServiceTest {
     public void setUp(){
         this.loginService = new LoginService();
         this.user = getUser();
+        this.blockedUser = getBlockedUser();
+
         Assert.assertEquals(3, user.getLoginAttempts());
         Assert.assertFalse(user.isBlocked());
+
+        Assert.assertEquals(0, blockedUser.getLoginAttempts());
+        Assert.assertTrue(blockedUser.isBlocked());
     }
 
     @Test
@@ -68,10 +74,23 @@ public class LoginServiceTest {
         Assert.assertEquals(0, user.getLoginAttempts());
         Assert.assertTrue(user.isBlocked());
     }
+    @Test
+    public void blockedUserLogin_ShouldReturnFalse() {
+        boolean actualResult = loginService.login(blockedUser, positiveUserInput);
+        Assert.assertFalse(actualResult);
+    }
 
     private User getUser() {
         User user = new User();
         user.setPassword("password");
+        return user;
+    }
+
+    private User getBlockedUser() {
+        User user = new User();
+        user.setPassword("password");
+        user.setLoginAttempts(0);
+        user.setBlocked(true);
         return user;
     }
 
