aa99c9f2e84cae07615273e63801561e6bcc9eaa
Alexandr Raby <riabyi.alexandr@gmail.com>
2021-01-26 17:03:08 +0300

    adds checkUserPassword

--- homework/java/alexandr_raby/LoginFeature/src/main/java/by/jrr/service/LoginService.java
+++ homework/java/alexandr_raby/LoginFeature/src/main/java/by/jrr/service/LoginService.java
@@ -1,4 +1,11 @@
 package by.jrr.service;
 
+import by.jrr.bean.User;
+
+import java.nio.charset.StandardCharsets;
+
 public class LoginService {
+    public boolean checkUserPassword(User user, String userInput) {
+    return user.getPassword().equals(userInput);
+    }
 }
