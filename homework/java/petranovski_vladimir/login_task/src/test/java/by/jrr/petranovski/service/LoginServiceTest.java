9b80638ac098a1527d87722932e2c5cff36172be
VladimirPetranovski <desantluninec@mail.ru>
2021-01-27 14:26:24 +0300

    adds initial attempts value

--- homework/java/petranovski_vladimir/login_task/src/test/java/by/jrr/petranovski/service/LoginServiceTest.java
+++ homework/java/petranovski_vladimir/login_task/src/test/java/by/jrr/petranovski/service/LoginServiceTest.java
@@ -6,17 +6,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class LoginServiceTest {
-
+    private User user;
     private LoginService test;
+
 
     @Before
     public void setUp() {
+
         this.test = new LoginService();
+        this.user = getUser();
+        Assert.assertEquals(3, user.getLoginAttempts());
     }
 
     @Test
     public void checkUserPasswordTrueTest() {
-        User user = getUser();
         String userInput = "password";
         boolean actualResult = test.checkUserPassword(user, userInput);
         Assert.assertTrue(actualResult);
@@ -24,7 +27,6 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPasswordFalseTest() {
-        User user = getUser();
         String userInput = "passw";
         boolean actualResult = test.checkUserPassword(user, userInput);
         Assert.assertFalse(actualResult);
