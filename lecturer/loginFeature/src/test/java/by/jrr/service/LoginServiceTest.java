c95ecf2e6efe58f67c73bcb07f2299a7aea7afcf
Asus <sveta.romasho@inbox.ru>
2021-01-27 16:04:17 +0300

    final refactoring

--- lecturer/loginFeature/src/test/java/by/jrr/service/LoginServiceTest.java
+++ lecturer/loginFeature/src/test/java/by/jrr/service/LoginServiceTest.java
@@ -28,7 +28,7 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPassword_positive(){
-        boolean actualResult = loginService.checkUserPassword(user, positiveUserInput);
+        boolean actualResult = loginService.login(user, positiveUserInput);
         Assert.assertTrue(actualResult);
     }
 
@@ -36,7 +36,7 @@ public class LoginServiceTest {
 
     @Test
     public void checkUserPassword_negative(){
-        boolean actualResult = loginService.checkUserPassword(user, negativeUserInput);
+        boolean actualResult = loginService.login(user, negativeUserInput);
         Assert.assertFalse(actualResult);
     }
 
@@ -49,7 +49,7 @@ public class LoginServiceTest {
 
     @Test
     public void reduceLoginAttempts(){
-        loginService.reduceLoginAttempts(user);
+        loginService.login(user, negativeUserInput);
         Assert.assertEquals(2, user.getLoginAttempts());
     }
 
@@ -68,7 +68,8 @@ public class LoginServiceTest {
 
     @Test
     public void blockUser(){
-        loginService.blockUser(user);
+        user.setLoginAttempts(1);
+        loginService.login(user, negativeUserInput);
         Assert.assertTrue(user.isBlocked());
     }
 
@@ -106,7 +107,7 @@ public class LoginServiceTest {
     @Test
     public void restoreAttempts(){
         user.setLoginAttempts(1);
-        loginService.restoreAttempts(user);
+        loginService.login(user, positiveUserInput);
         Assert.assertEquals(3, user.getLoginAttempts());
     }
 
