f9728258246c8728467a2db3126aa258a0102880
VladimirPetranovski <desantluninec@mail.ru>
2021-01-27 12:53:54 +0300

    adds checkUserPassword functionality

--- homework/java/petranovski_vladimir/login_task/src/main/java/by/jrr/petranovski/service/LoginService.java
+++ homework/java/petranovski_vladimir/login_task/src/main/java/by/jrr/petranovski/service/LoginService.java
@@ -1,5 +1,10 @@
 package by.jrr.petranovski.service;
 
+import by.jrr.petranovski.bean.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- homework/java/petranovski_vladimir/login_task/src/test/java/by/jrr/petranovski/service/LoginServiceTest.java
+++ homework/java/petranovski_vladimir/login_task/src/test/java/by/jrr/petranovski/service/LoginServiceTest.java
@@ -1,8 +1,39 @@
 package by.jrr.petranovski.service;
 
-import static org.junit.Assert.*;
+import by.jrr.petranovski.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
 
+    private LoginService test;
+
+    @Before
+    public void setUp() {
+        this.test = new LoginService();
+    }
+
+    @Test
+    public void checkUserPasswordTrueTest() {
+        User user = getUser();
+        String userInput = "password";
+        boolean actualResult = test.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void checkUserPasswordFalseTest() {
+        User user = getUser();
+        String userInput = "passw";
+        boolean actualResult = test.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 
 }
