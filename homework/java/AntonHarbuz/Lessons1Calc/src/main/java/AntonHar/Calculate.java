31e0af8e7c92e47fc0e0cb5851a851cd1c26bea9
Anton Harbuz <4ack90@gmail.com>
2021-01-30 22:50:10 +0300

    creat calculate

---
+++ homework/java/AntonHarbuz/Lessons1Calc/src/main/java/AntonHar/Calculate.java
@@ -0,0 +1,28 @@
+package AntonHar;
+
+public class Calculate {
+    public static double calculate (int a, int b, String command){
+        double result = 0.0;
+        switch (command){
+            case "add":
+                result = new Addition(a,b).catculate();
+                break;
+            case "div":
+                result = new Division(a,b).catculate();
+                break;
+            case "mult":
+                result = new Multiplication(a,b).catculate();
+                break;
+            case "rem":
+                result = new Remalder(a,b).catculate();
+                break;
+            case "sub":
+                result = new Subtraction(a,b).catculate();
+                break;
+            default:
+                System.out.println("Operation not supported, default 0.0 has been returned");
+                break;
+        }
+        return result;
+    }
+}
---
+++ homework/java/AntonHarbuz/Lessons1Calc/src/main/java/AntonHar/Subtraction.java
@@ -0,0 +1,17 @@
+package AntonHar;
+
+public class Subtraction {
+
+    private int a;
+    private int b;
+
+    public Subtraction(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int catculate(){
+        return a - b;
+    }
+
+}
