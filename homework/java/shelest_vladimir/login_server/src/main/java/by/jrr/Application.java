a7ddbbecdcb2b4cf4e35ad7a6f9098e87d4dbe2b
ShelestV <starvova2002@gmail.com>
2021-01-30 19:12:19 +0200

    Return body of main function

--- homework/java/shelest_vladimir/login_server/src/main/java/by/jrr/Application.java
+++ homework/java/shelest_vladimir/login_server/src/main/java/by/jrr/Application.java
@@ -6,5 +6,9 @@ import by.jrr.service.UserService;
 public class Application {
 
     public static void main(String[] args) {
+        User user = new User("Login", "Password");
+        UserService service = new UserService();
+
+        service.logIn(user, "Login", "Password");
     }
 }
