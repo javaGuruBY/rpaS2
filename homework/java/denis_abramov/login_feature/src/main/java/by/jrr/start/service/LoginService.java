c813b4a4a8827a5886a1ea6800505228222abf2b
DenisAbramovDm <sdgnhfs@gmail.com>
2021-01-31 20:51:34 +0300

    added reduce login attempts on wrong password

---
+++ homework/java/denis_abramov/login_feature/src/main/java/by/jrr/start/service/LoginService.java
@@ -0,0 +1,20 @@
+package by.jrr.start.service;
+
+import by.jrr.start.bean.User;
+
+public class LoginService {
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+
+    }
+
+    public void reduceLoginAttempts(User user) {
+        user.setLoginAttempts(user.getLoginAttempts() - 1);
+
+    }
+
+    public boolean login(User user, String userInput) {
+        reduceLoginAttempts(user);
+        return checkUserPassword(user, userInput);
+    }
+}
