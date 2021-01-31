ef82af2568fb58c7625921c3e54ba50cd33df4c0
Elena <len.owa@yandex.ru>
2021-01-31 19:01:58 +0300

    adds checkUserPassword functionality

--- homework/java/Owa_E/loginfeature/src/main/java/by/jrr/service/LoginService.java
+++ homework/java/Owa_E/loginfeature/src/main/java/by/jrr/service/LoginService.java
@@ -1,4 +1,10 @@
 package by.jrr.service;
 
+import by.jrr.bean.User;
+
 public class LoginService {
+
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
