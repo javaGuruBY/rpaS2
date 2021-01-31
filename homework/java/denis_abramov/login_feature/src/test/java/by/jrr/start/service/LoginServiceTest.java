f96c92e57d611ce4ac9c10e64330c19aa5e64782
DenisAbramovDm <sdgnhfs@gmail.com>
2021-01-31 20:13:23 +0300

    added checkUserPassword functionality

--- homework/java/denis_abramov/login_feature/src/test/java/by/jrr/start/service/LoginServiceTest.java
+++ homework/java/denis_abramov/login_feature/src/test/java/by/jrr/start/service/LoginServiceTest.java
@@ -1,7 +1,48 @@
 package by.jrr.start.service;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import by.jrr.start.bean.User;
+
 
 public class LoginServiceTest {
+    LoginService loginService;
+    @Before
+    public void setUp() {
+        this.loginService = new LoginService();
+    }
+
+    @Test
+    public void checkUserPassword_positive() {
+        User user = getUser();
+
+        String userInput = "password";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+
+
+
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
+
+    @Test
+    public void checkUserPassword_negative() {
+        User user = getUser();
+
+        String userInput = "wrong";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+
+
+
+    }
 
 }
