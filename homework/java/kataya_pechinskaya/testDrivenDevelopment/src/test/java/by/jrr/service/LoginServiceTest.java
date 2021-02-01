e629d4ce2f143e18c3fb1881f42d5463c08395d8
kate-pechinskaya <katttka98@gmail.com>
2021-02-01 18:20:08 +0300

    adds reduce login attemps om wrong password

--- homework/java/kataya_pechinskaya/testDrivenDevelopment/src/test/java/by/jrr/service/LoginServiceTest.java
+++ homework/java/kataya_pechinskaya/testDrivenDevelopment/src/test/java/by/jrr/service/LoginServiceTest.java
@@ -20,17 +20,35 @@ public class LoginServiceTest {
     @Test
     public  void checkUserPassword_positive() {
         String userInput = "password";
-        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        boolean actualResult = loginService.login(user, userInput);
         Assert.assertTrue(actualResult);
     }
 
     @Test
     public  void checkUserPassword_negative() {
         String userInput = "wrong";
-        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        boolean actualResult = loginService.login(user, userInput);
         Assert.assertFalse(actualResult);
     }
 
+    @Test
+    public void receLoginAttempts(){
+        loginService.reduceLoginAttempts(user);
+        Assert.assertEquals(2, user.getLoginAttemps());
+    }
+    @Test
+    public  void login_positive(){
+        String userInput = "password";
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+    @Test
+    public  void login_negative() {
+        String userInput = "wrong";
+        boolean actualResult = loginService.login(user, userInput);
+        Assert.assertFalse(actualResult);
+        Assert.assertEquals(2, user.getLoginAttemps());
+    }
     private User getUser() {
         User user = new User();
         user.setPassword("password");
