4cd99c0b08e81dbc52a68a022305a351082abc6e
Yury Fyodorov <fedoruch2000@gmail.com>
2021-02-04 12:47:34 +0300

    add block user after 3 wrong passwords

--- homework/java/fyodorov/login_feature/src/test/java/by/jrr/start/service/LoginServiceTest.java
+++ homework/java/fyodorov/login_feature/src/test/java/by/jrr/start/service/LoginServiceTest.java
@@ -9,21 +9,23 @@ public class LoginServiceTest {
 
     LoginService loginService;
     User user;
+    String positiveUserInput = "password";
+    String negativeUserInput = "wrong";
 
     @Before
     public void setUp (){
         this.loginService = new LoginService();
         this.user = getUser();
         Assert.assertEquals(3, user.getLoginAttempts());
+        Assert.assertFalse(user.isBlocked());
     }
 
     @Test
     public void checkUserPassword_positive(){
         User user = getUser();
         Assert.assertEquals(3, user.getLoginAttempts());
-        String userInput = "password";
 
-        boolean actualResult = loginService.checkUserPassword (user, userInput);
+        boolean actualResult = loginService.checkUserPassword (user, positiveUserInput);
         Assert.assertTrue(actualResult);
 
     }
@@ -32,9 +34,8 @@ public class LoginServiceTest {
     public void checkUserPassword_negative() {
         User user = getUser();
         Assert.assertEquals(3, user.getLoginAttempts());
-        String userInput = "wrong";
 
-        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        boolean actualResult = loginService.checkUserPassword(user, negativeUserInput);
         Assert.assertFalse(actualResult);
     }
 
@@ -46,18 +47,38 @@ public class LoginServiceTest {
 
     @Test
     public void login_positive(){
-        String userInput = "password";
 
-        boolean actualResult = loginService.login (user, userInput);
+        boolean actualResult = loginService.login (user, positiveUserInput);
         Assert.assertTrue(actualResult);
     }
     @Test
     public void login_negative(){
-        String userInput = "wrong";
 
-        boolean actualResult = loginService.login (user, userInput);
+        boolean actualResult = loginService.login (user, negativeUserInput);
         Assert.assertFalse(actualResult);
         Assert.assertEquals(2, user.getLoginAttempts());
+    }
+
+    @Test
+    public void blockUser (){
+        loginService.blockUser (user);
+        Assert.assertTrue(user.isBlocked());
+    }
+
+    @Test
+    public void after3WrongPasswords_ShouldBlockUser(){
+
+        loginService.login(user, negativeUserInput);
+        Assert.assertFalse(user.isBlocked());
+        Assert.assertEquals(2, user.getLoginAttempts());
+
+        loginService.login(user, negativeUserInput);
+        Assert.assertFalse(user.isBlocked());
+        Assert.assertEquals(1, user.getLoginAttempts());
+
+        loginService.login(user, negativeUserInput);
+        Assert.assertEquals(0, user.getLoginAttempts());
+        Assert.assertTrue(user.isBlocked());
     }
 
     private User getUser() {
