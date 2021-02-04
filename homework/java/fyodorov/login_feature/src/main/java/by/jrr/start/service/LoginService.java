89c270e943f437cee682141455e318396f7e3375
Yury Fyodorov <fedoruch2000@gmail.com>
2021-02-04 13:19:31 +0300

    restore number attempts

--- homework/java/fyodorov/login_feature/src/main/java/by/jrr/start/service/LoginService.java
+++ homework/java/fyodorov/login_feature/src/main/java/by/jrr/start/service/LoginService.java
@@ -20,10 +20,18 @@ public class LoginService {
             blockUser(user);
         }
         reduceLoginAttempts(user);
-        return checkUserPassword(user, userInput);
+        boolean result = checkUserPassword(user, userInput);
+        if (result){
+            restoreAttempts(user);
+        }
+        return result;
     }
 
     public void blockUser(User user) {
         user.setBlocked(true);
+    }
+
+    public void restoreAttempts(User user) {
+        user.setLoginAttempts(3);
     }
 }
